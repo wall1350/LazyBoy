eb0d555a1cd15b6a2aa0e390e194403be6ec662b
Inyouchange <bettychen84@gmail.com>
2018-12-19 00:14:47 +0800

    Update Outfit

--- app/src/main/java/com/example/jesuiswilliam/lazyboy/OutfitItem.java
+++ app/src/main/java/com/example/jesuiswilliam/lazyboy/OutfitItem.java
@@ -1,24 +1,38 @@
 package com.example.jesuiswilliam.lazyboy;
 
 import android.content.Context;
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.constraint.ConstraintLayout;
-import android.support.v4.app.Fragment;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ImageButton;
+import android.widget.Button;
 
-public class OutfitItem extends PageView{
-    public OutfitItem(Context context) {
+public class OutfitItem extends PageView {
+
+    private ImageButton btnJacket;
+
+    public OutfitItem(final Context context) {
         super(context);
         View view = LayoutInflater.from(context).inflate(R.layout.outfititem, null);
+
+        btnJacket = view.findViewById(R.id.btnJacket);
+        btnJacket.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Intent intent = new Intent();
+                intent.setClass(context,Test.class);
+                context.startActivity(intent);
+            }
+        });
+
         addView(view);
+
     }
 
     @Override
     public void refresh() {
     }
-}
 
+}
--- app/src/main/java/com/example/jesuiswilliam/lazyboy/OutfitOutfit.java
+++ app/src/main/java/com/example/jesuiswilliam/lazyboy/OutfitOutfit.java
@@ -2,23 +2,33 @@ package com.example.jesuiswilliam.lazyboy;
 
 
 import android.content.Context;
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.constraint.ConstraintLayout;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
+import android.widget.ImageButton;
 
 public class OutfitOutfit extends PageView {
 
-    public OutfitOutfit(Context context) {
+    private ImageButton btnRelax, btnStreet, btnGen, btnJob;
+
+    public OutfitOutfit(final Context context) {
         super(context);
         View view = LayoutInflater.from(context).inflate(R.layout.outfitoutfit, null);
+
+        btnRelax = view.findViewById(R.id.btnRelax);
+        btnRelax.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Intent intent = new Intent();
+                intent.setClass(context,OutfitRelax.class);
+                context.startActivity(intent);
+            }
+        });
+
         addView(view);
+
     }
 
     @Override
